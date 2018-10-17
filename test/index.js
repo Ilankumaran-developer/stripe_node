ef89bf3119d4a76e32f5c9a190eef35f93c9bec3
ilanthedev <ilankumaran.1@partner.samsung.com>
2018-10-17 19:20:16 +0530

    done

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const stripe = require('../index.js')
-const stripe_it = new stripe({key:'sk_test_2kSmiYiONir5vpPRvby1RoJg'})
+const stripe_it = new stripe({ key: 'sk_test_2kSmiYiONir5vpPRvby1RoJg' })
 
-let mocha  = require('mocha');
+let mocha = require('mocha');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should()
@@ -9,10 +9,35 @@ let assert = require('assert')
 
 chai.use(chaiHttp)
 
-describe('customerslist',()=>{
-    it('should list all customers',async (done)=>{
-       let customer = await stripe_it._.customersList({limit:3});
-       assert.notEqual(customer.data.length,0,'these are not')
-       done();
+describe('customerslist', () => {
+    it('should list all customers', async () => {
+        return new Promise(async(resolve, reject) => {
+            let customer = await stripe_it._.customersList({ limit: 3 });
+            assert.notEqual(customer.length, 0, 'these are not')
+            resolve();
+        })
+
     })
 })
+describe('addingCustomer',()=>{
+    it('Should add a new customer',async ()=>{
+        return new Promise(async(resolve,reject)=>{
+            let token = await stripe_it._.createToken({
+                card: {
+                  "number": '4242424242424242',
+                  "exp_month": 12,
+                  "exp_year": 2019,
+                  "cvc": '123'
+                }});
+                console.log(token)
+    
+            let newCustomer = await stripe_it._.createCustomer({
+                description: 'Customer for jenny.rosen@example.com',
+                source: token.id // obtained with Stripe.js
+              })
+              console.log(newCustomer)
+              resolve();
+            })
+        })
+        
+    })
